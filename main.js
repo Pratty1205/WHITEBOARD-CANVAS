944554ceb2a180da6089ab2b323831c66b090dfb
Pratty1205 <78639572+Pratty1205@users.noreply.github.com>
2021-04-10 22:42:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,42 +1,58 @@
-var canvas = document.getElementById("myCanvas");
+var last_position_of_x, last_position_of_y;
+color = "black";
+width_of_line = 2;
+
+canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext("2d");
-var mouseEvent = "empty";
-var last_position_of_x,
-var last_position_of_y;
-var color = "black";
-var width = 1;
-canvas.addEventListener("mousedown", my_mousedown);
 
-function my_mousedown(e) {
-    var color = document.getElementById("color").value;
-    var width = document.getElementById("width_of_line").value;
-    var mouseEvent = "mousedown"
-};
+var width = screen.width;
 
-canvas.addEventListener("mouseup", my_mouseup);
+new_width =  screen.width - 70; 
+new_height = screen.height - 300;
+	if(width < 992)
+	{
+	document.getElementById("myCanvas").width = new_width;
+    document.getElementById("myCanvas").height = new_height;
+    document.body.style.overflow = "hidden";
+	}
+	canvas.addEventListener("touchstart", my_touchstart);
 
-function my_mouseup(e) {
-    var mouseEvent = "my_mouseup";
-};
-canvas.addEventListener("mouseleave", my_mouseleave);
+function my_touchstart(e) 
+{
+	console.log("my_touchstart");
+  //Additional Activity
+  color = document.getElementById("color").value;
+  width_of_line = document.getElementById("width_of_line").value;
+  //End Additional Activity
+         
+    last_position_of_x = e.touches[0].clientX - canvas.offsetLeft;
+    last_position_of_y = e.touches[0].clientY - canvas.offsetTop;
+}
 
-function my_mouseleave(e) {
-    var mouseEvent = "my_mouseleave";
-};
+canvas.addEventListener("touchmove", my_touchmove);
+function my_touchmove(e) 
+{
 
-canvas.addEventListener("mousemove", my_mousemove);
+	console.log("my_touchMove");
+    current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
+    current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
 
-function my_mousemove(e) {
-    var current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
-    var current_position_of_mouse_y = e.clientY - canvas.offsetTop;
-    if (mouseEvent == mousedown) {
-        ctx.beginPath()
-        ctx.strokeStyle = "color"
-        ctx.lineWidth = "width"
-        ctx.moveTo("last_position_of_x, last_position_of_y");
-        ctx.lineTo("current_position_of_mouse_x, current_position_of_mouse_y");
-        ctx.stroke()
-        var last_position_of_x = "current_position_of_mouse_x";
-        var last_position_of_y = "current_position_of_mouse_y";
-    }
+    // old same old as the paint web app
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width_of_line;
+
+    console.log("Last position of x and y coordinates = ");
+    console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
+    ctx.moveTo(last_position_of_x, last_position_of_y);
+
+    console.log("Current position of x and y coordinates = ");
+    console.log("x  = " + current_position_of_touch_x + "y = " + current_position_of_touch_y);
+    ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
+    ctx.stroke();
+
+    last_position_of_x = current_position_of_touch_x; 
+    last_position_of_y = current_position_of_touch_y;
+    
+    // end of old same old as the paint web app
 }
