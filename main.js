edeb1ecc835d69036599eaf583870dad5619fcc5
Pratty1205 <78639572+Pratty1205@users.noreply.github.com>
2021-04-13 22:40:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,58 +1,44 @@
-var last_position_of_x, last_position_of_y;
-color = "black";
-width_of_line = 2;
-
-canvas = document.getElementById('myCanvas');
-ctx = canvas.getContext("2d");
-
-var width = screen.width;
-
-new_width =  screen.width - 70; 
-new_height = screen.height - 300;
-	if(width < 992)
-	{
-	document.getElementById("myCanvas").width = new_width;
-    document.getElementById("myCanvas").height = new_height;
-    document.body.style.overflow = "hidden";
-	}
-	canvas.addEventListener("touchstart", my_touchstart);
-
-function my_touchstart(e) 
-{
-	console.log("my_touchstart");
-  //Additional Activity
-  color = document.getElementById("color").value;
-  width_of_line = document.getElementById("width_of_line").value;
-  //End Additional Activity
-         
-    last_position_of_x = e.touches[0].clientX - canvas.offsetLeft;
-    last_position_of_y = e.touches[0].clientY - canvas.offsetTop;
+var canvas=document.getElementById("mycanvas");
+ctx=canvas.getcontext("2d");
+rover_width=100;
+rover_height=90;
+rover_x=10;
+rover_y=10;
+background_image="mars.jpg";
+rover_image="rover.png";
+function add(){
+    background_image_t=new Image();
+    background_image_t.onload=uploadBackground;
+    background_image_t.src=background_image;
+    rover_image_t=new Image();
+    rover_image_t.onload=uploadRover;
+    rover_image_t.src=rover_image;
 }
-
-canvas.addEventListener("touchmove", my_touchmove);
-function my_touchmove(e) 
-{
-
-	console.log("my_touchMove");
-    current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
-    current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
-
-    // old same old as the paint web app
-    ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.lineWidth = width_of_line;
-
-    console.log("Last position of x and y coordinates = ");
-    console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
-    ctx.moveTo(last_position_of_x, last_position_of_y);
-
-    console.log("Current position of x and y coordinates = ");
-    console.log("x  = " + current_position_of_touch_x + "y = " + current_position_of_touch_y);
-    ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
-    ctx.stroke();
-
-    last_position_of_x = current_position_of_touch_x; 
-    last_position_of_y = current_position_of_touch_y;
-    
-    // end of old same old as the paint web app
+function uploadBackground(){
+    ctx.drawImage(background_image_t,0,0,canvas.width,canvas.height);
+}
+function uploadRover(){
+    ctx.drawImage(rover_image_t,rover_x,rover_y,rover_width,rover_height);
+}
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e){
+    keyPress=e.keyCode;
+    console.log(keyPress);
+    if (keyPress=="38") {
+        up();
+        console.log("up");
+    }
+    if (keyPress==40) {
+        down();
+        console.log("down");
+    }
+    if (keyPress==39) {
+        right();
+        console.log("right");
+    }
+    if (keyPress==37) {
+        left();
+        console.log(left);
+    }
+       
 }
